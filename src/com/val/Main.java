b0b1095cf148344ccc4c1f5f239235738b5a9260
Valeria Paz <21214@student.dorset-college.ie>
2020-10-20 12:59:28 +0100

    first commit

---
+++ src/com/val/Main.java
@@ -0,0 +1,13 @@
+package com.val;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] item = {"apple", "orange"};
+        Double [] price = {0.60, 0.25};
+
+        
+
+    }
+}
