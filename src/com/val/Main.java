5f1deb67c922d60ef6a28f5fe6dbde6b071625d8
Valeria Paz <21214@student.dorset-college.ie>
2020-10-20 13:13:33 +0100

    sec commit

--- src/com/val/Main.java
+++ src/com/val/Main.java
@@ -1,13 +1,25 @@
 package com.val;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String [] item = {"apple", "orange"};
-        Double [] price = {0.60, 0.25};
+        ArrayList<Items> items = new ArrayList<Items>();
 
-        
+        items.add(apple);
+        items.add(orange);
+
+        Double [] price = {0.60, 0.25};
+        int quantity = 0;
+        double sum = 0;
+
+        for (int i = 0; i< items.length; i++) {
+            sum+=price[i] * quantity;
+        }
+
+        System.out.println(sum);
 
     }
 }
