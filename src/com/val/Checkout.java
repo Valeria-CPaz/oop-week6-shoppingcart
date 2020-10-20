e731f795a4ca006a3ec9c3c93eeb2f736c4ae9ee
Valeria Paz <21214@student.dorset-college.ie>
2020-10-20 13:43:48 +0100

    third commit

--- src/com/val/Checkout.java
+++ src/com/val/Checkout.java
@@ -5,5 +5,9 @@ public class Checkout {
     String[] item = {"apple", "orange"};
     double[] price = {0.60, 0.25};
 
-    
+    public Checkout(String[] item, double[] price){
+        this.item[] = item[];
+        this.price = price;
+    }
+
 }
